4251e835325ab9df3fbcaa183a58b895bb7f700a
Ariyo <alokhalder6359@gmail.com>
2020-11-15 12:51:30 +0530

    Not complete yet

--- main.py
+++ main.py
@@ -15,9 +15,12 @@ print(f"""
 
 <title>
 IIT Kolkata
-</title>
+</title> 
 <h1 class='center '>Welcome {name} To IIT Kolkata </h1>
+<div class='box'><h2> Welcome {name} To Our College Portal</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem laudantium tenetur recusandae officiis porro, repellat, eius quas earum deserunt sapiente ut vel! Asperiores quaerat commodi nihil fuga aut quas facere temporibus? Quia, delectus. Distinctio! </p> </div>
+<div class='box'><h2>!!! {name} Best Of Luck For Exams!!!</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem laudantium tenetur recusandae officiis porro, repellat, eius quas earum deserunt sapiente ut vel! Asperiores quaerat commodi nihil fuga aut quas facere temporibus? Quia, delectus. Distinctio! </p> </div>
 <script src="https://kit.fontawesome.com/2f47edd958.js" crossorigin="anonymous"></script>
 """)
 style.close()
+
 nav.close()
---
+++ profile.py
@@ -0,0 +1,25 @@
+#!C:\python 3.8\python.exe
+print("Content-type:text/html\r\n\r\n")
+import cgi
+import mysql.connector
+db=mysql.connector.connect(host="127.0.0.1",user="root",password="",db="college")
+cursor=db.cursor()
+data=cgi.FieldStorage()
+roll=int(data.getvalue('roll'))
+sql_query="select * from students where roll='%d'"%(roll)
+cursor.execute(sql_query)
+result=cursor.fetchone()
+db.commit()
+# print(result)
+print(f"""
+<h1> Welcome {result[0]}</h1>
+<link rel="stylesheet" href="/college/style.css">
+<form>
+<input type="emai" name="email" value="{result[1]}" placeholder="Your email here">
+<input type="name" name="name"value="{result[0]}"placeholder="Your Name here">
+<input type="text" name="stream" value="{result[2]}" placeholder="Your stream here">
+<input type="password" name="password" value="{result[3]}" placeholder="Your Password here">
+<button class="btn" type="submit">Change</button>
+<a href="/college/main.py">Cancel</a>
+</form>
+""")
